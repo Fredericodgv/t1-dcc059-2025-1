1b490ec08ab3656795fd80010a8932195223614d
fredericodgv <fredericodgv@gmail.com>
2025-08-05 10:44:40 -0300

    Corrige algoritmo

--- src/AlgoritmosGulosos.h
+++ src/AlgoritmosGulosos.h
@@ -10,7 +10,7 @@ public:
     static Grafo *conjunto_dominante_randomizado(Grafo *grafo, float alfa = 0.5f);
     static Grafo *conjunto_dominante_reativo(Grafo *grafo, vector<float> &alfas, int iteracoes, int tamanho_bloco);
     static Grafo* executar_randomizado_n_vezes(Grafo* grafo, int n_iteracoes, float alfa);
-    Grafo* executar_reativo_n_vezes(const Grafo* grafo, int n_execucoes, std::vector<float>& alfas, int iteracoes_reativo, int tamanho_bloco);
+    static Grafo* executar_reativo_n_vezes(Grafo* grafo, int n_execucoes, std::vector<float>& alfas, int iteracoes_reativo, int tamanho_bloco);
 };
 
 #endif // ALGORITMOS_GULOSOS_H
