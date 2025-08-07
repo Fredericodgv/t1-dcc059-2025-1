2dc2cb0fd0ec0a3f071f5eb495407390e61c6b8d
fredericodgv <fredericodgv@gmail.com>
2025-08-07 13:50:21 -0300

    Renomeia função conjunto_dominante para conjunto_dominante_conexo e ajusta lógica para tratamento de grafos vazios

--- src/AlgoritmosGulosos.h
+++ src/AlgoritmosGulosos.h
@@ -6,11 +6,11 @@
 class AlgoritmosGulosos
 {
 public:
-    static std::vector<char> conjunto_dominante(Grafo *grafo);
+    static std::vector<char> conjunto_dominante_conexo(Grafo *grafo);
     static std::vector<char> conjunto_dominante_randomizado(Grafo *grafo, float alfa = 0.5f);
     static std::vector<char> conjunto_dominante_reativo(Grafo *grafo, vector<float> &alfas, int iteracoes, int tamanho_bloco);
-    static std::vector<char> executar_randomizado_n_vezes(Grafo* grafo, int n_iteracoes, float alfa);
-    static std::vector<char> executar_reativo_n_vezes(Grafo* grafo, int n_execucoes, std::vector<float>& alfas, int iteracoes_reativo, int tamanho_bloco);
+    static std::vector<char> executar_randomizado_n_vezes(Grafo *grafo, int n_iteracoes, float alfa);
+    static std::vector<char> executar_reativo_n_vezes(Grafo *grafo, int n_execucoes, std::vector<float> &alfas, int iteracoes_reativo, int tamanho_bloco);
 };
 
 #endif // ALGORITMOS_GULOSOS_H
